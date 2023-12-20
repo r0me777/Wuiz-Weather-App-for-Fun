c9b5e99db8758ea0977e2b0057fb90cc9c6515f9
r0me777 <58448938+r0me777@users.noreply.github.com>
2023-12-20 14:31:41 -0600

    Added, WeatherPage, Updated Homepage
    I updated the Homepage to look more like the intial draft, I also, made a weatherpage.py file, this file needs to be made from the zipcode/location information inputting into the homepages entry box, so I'll have to look into it, but other than that its just basic changes.

--- Homepage.py
+++ Homepage.py
@@ -1,9 +1,25 @@
 from tkinter import *
 root = Tk()
 
-mylabel = Label(root, text="Hello World!", fg="red")
+#main intital window, going to comprise of big font of name of app: WUIZ
+#Smaller text below is going to say: "Search where you want to see your Weather"
+#Below this text there is going to be a search bar. (Input Bar)
+
+root.geometry("700x700")
+root.title('Wuiz')
+
+mylabel = Label(root, text="WUIZ", fg="black", font=("Helvetica", 50), padx=50,pady=50)
+mylabel2 = Label(root, text="Search where you want to see your Weather", fg="black", font=("Helvetica", 20), padx=50,pady=50)
+searchinput = Entry(root,background='light grey',borderwidth=5, width=50,)
+
+def funcclickzip(event):
+    print("Zip Code is: " + searchinput.get()) #Gets value of input box when you press enter button
+
+root.bind('<Return>', funcclickzip) #very rought but still
+
 
 mylabel.pack()
+mylabel2.pack()
+searchinput.pack()
 
 root.mainloop()
-
---
+++ Weatherpage.py
@@ -0,0 +1,4 @@
+from tkinter import *
+root = Tk()
+
+#Should be generated based of zip code of Homepage, will look into python weather module.

09e0da3c2ff1fcf75ba31ac3f9bb0d9ab40ff014
r0me777 <58448938+r0me777@users.noreply.github.com>
2023-12-22 16:57:04 -0600

    Added a requirements file, and image files
    Added a requirements file which holds packages made to access and use image file.
    
    Co-Authored-By: rlinskyy <99920552+rlinskyy@users.noreply.github.com>

--- Homepage1.py
+++ Homepage1.py
@@ -1,4 +1,6 @@
 from tkinterappfile import *
+from PIL import Image, ImageTk
+
 
 class Homepage(tk.Frame):
     def __init__(self,parent):
@@ -13,20 +15,29 @@ class Homepage(tk.Frame):
                 mylabel.config(bg="black", fg="white")
                 mylabel2.config(bg="black", fg="white")
                 searchinput.config(bg="dark grey", fg="white")
+                themeButton.config(image=moonIcon, text="Dark Mode")
                 currentTheme = "dark"
             else:
                 self.config(bg="white")
                 mylabel.config(bg="white", fg="black")
                 mylabel2.config(bg="white", fg="black")
                 searchinput.config(bg="light grey", fg="black")
+                themeButton.config(image=sunIcon, text="Light Mode")
                 currentTheme = "light"
 
         currentTheme = "light"
 
+        sunImage = Image.open("Pngs/sun.png").resize((30, 30))
+        moonImage = Image.open("Pngs/moon.png").resize((30, 30))
+
+        sunIcon = ImageTk.PhotoImage(sunImage)
+        moonIcon = ImageTk.PhotoImage(moonImage)
+
         # label1 = tk.Label(self,background="White").pack(fill="both", expand=True)
 
         # Labels and Search Box in Homepage
-        themeButton = Button(self, text="Change Theme", command=changeTheme)
+        themeButton = Button(self, text="Light Mode", command=changeTheme)
+        themeButton.config(image=sunIcon, text="Light Mode")
         mylabel = tk.Label(self, text="WUIZ", fg="black", font=("Helvetica", 50), padx=50, pady=50)
         mylabel2 = tk.Label(self, text="Search where you want to see your Weather", fg="black", font=("Helvetica", 20),
                             padx=50, pady=50)
@@ -34,7 +45,8 @@ class Homepage(tk.Frame):
 
         # moves the button to the top right corner
 
-        themeButton.pack(side="top", anchor="ne", padx=10, pady=10)  # packs color button into frame
+        themeButton.pack(side="top", anchor="ne", padx=10, pady=10)
+        # packs color button into frame
         mylabel.pack()  # packs label into frame
         mylabel2.pack()  # packs label into frame
         searchinput.pack()  # packs search into frame

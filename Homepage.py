03a13fbe204265eed2298a20d66d481dc0cda826
rlinskyy <99920552+rlinskyy@users.noreply.github.com>
2023-12-21 19:28:14 -0600

    Added icons for light/dark mode

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,14 @@
 from tkinter import *
+from PIL import Image, ImageTk
 root = Tk()
 
 currentTheme = "light"
+
+sunImage = Image.open("Pngs/sun.png").resize((30, 30))
+moonImage = Image.open("Pngs/moon.png").resize((30, 30))
+
+sunIcon = ImageTk.PhotoImage(sunImage)
+moonIcon = ImageTk.PhotoImage(moonImage)
 
 # changes the color of the background b/w light and dark mode
 def changeTheme():
@@ -11,12 +18,14 @@ def changeTheme():
         mylabel.config(bg = "black", fg = "white")
         mylabel2.config(bg = "black", fg = "white")
         searchinput.config(bg = "dark grey", fg = "white")
+        themeButton.config(image = moonIcon, text = "Dark Mode")
         currentTheme = "dark"
     else:
         root.config(bg = "white")
         mylabel.config(bg = "white", fg = "black")
         mylabel2.config(bg = "white", fg = "black")
         searchinput.config(bg = "light grey", fg = "black")
+        themeButton.config(image = sunIcon, text = "Light Mode")
         currentTheme = "light"
 
 #main intital window, going to comprise of big font of name of app: WUIZ
@@ -31,9 +40,10 @@ mylabel2 = Label(root, text="Search where you want to see your Weather", fg="bla
 searchinput = Entry(root,background='light grey',borderwidth=5, width=50,)
 
 # creates the button that changes the theme
-themeButton = Button(root, text = "Change Theme", command = changeTheme)
+themeButton = Button(root, text = "Light Mode", command = changeTheme)
 # moves the button to the top right corner
 themeButton.pack(side = TOP, anchor = NE, padx = 10, pady = 10)
+themeButton.config(image = sunIcon, text = "Light Mode")
 
 def funcclickzip(event):
     print("Zip Code is: " + searchinput.get())  #Gets value of input box when you press enter button
